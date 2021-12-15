64ef96eb7cbab1e8c97c59b890e3a0791e3037de
Egor <etretiak03@gmail.com>
2021-12-15 06:20:22 +0300

    add: class 'Main'

---
+++ src/mainPackage/Main.java
@@ -0,0 +1,10 @@
+package mainPackage;
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        MainFrame frame = new MainFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
